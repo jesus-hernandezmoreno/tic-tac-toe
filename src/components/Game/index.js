6d622017adfc77dad6b8078bb6403f60fa88e8aa
Web Wizard <jesus.hernandezmoreno@outlook.com>
2018-04-19 21:27:01 -0500

    UPDATE(assets): New logo

--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -82,6 +82,7 @@ class Game extends React.Component {
 
     return (
       <div className="game">
+        <img src="/logo_white.png"/>
         <div className="game-board">
           <Board
             squares={current.squares}
