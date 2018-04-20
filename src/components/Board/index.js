cd06f198cadca5c5b6026038b8411e09bceda42d
Web Wizard <jesus.hernandezmoreno@outlook.com>
2018-04-19 20:32:02 -0500

    STYLE(): Change board style and use flexbox

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -13,23 +13,23 @@ class Board extends React.Component {
 
   render() {
     return (
-      <table>
-        <tr>
+      <div className="ticTacToe">
+        <div className="column">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
           {this.renderSquare(2)}
-        </tr>
-        <tr>
+        </div>
+        <div className="column">
           {this.renderSquare(3)}
           {this.renderSquare(4)}
           {this.renderSquare(5)}
-        </tr>
-        <tr>
+        </div>
+        <div className="column">
           {this.renderSquare(6)}
           {this.renderSquare(7)}
           {this.renderSquare(8)}
-        </tr>
-      </table>
+        </div>
+      </div>
     );
   }
 }
--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -89,10 +89,10 @@ class Game extends React.Component {
           />
 
         </div>
-        <div className="game-info">
+        {/*<div className="game-info">
           <div>{status}</div>
           <ol>{moves}</ol>
-        </div>
+        </div>*/}
       </div>
     );
   }
