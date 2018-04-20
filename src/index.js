d226937195009d71ca8684322e6db824650c64ea
Web Wizard <jesus.hernandezmoreno@outlook.com>
2018-04-19 21:54:57 -0500

    STYLES(): Delete index.css and move to global-styles

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import Game from 'components/Game';
-
+// Import CSS reset and Global Styles
+import 'utils/global-styles';
 // ========================================
 
 ReactDOM.render(
